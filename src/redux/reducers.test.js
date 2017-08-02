fa7fcd19ef5591488d81a16013d43032b87e3f21
Mico Piira <mico.piira@gmail.com>
2017-08-02 13:16:26 +0300

    Add unit test for lights reducer

--- src/redux/reducers.test.js
+++ src/redux/reducers.test.js
@@ -1,3 +1,11 @@
-/**
- * Created by mico on 2.8.2017.
- */
+import { lights } from './reducers';
+import { setLightState } from './actions';
+
+
+
+it('sets light state', () => {
+    const initialState = {1: {state: {a: 'b', c: 'd'}}};
+    const wantedState = {1: {state: {a: 'a', c: 'd'}}};
+    const action = setLightState(1, {a: 'a'});
+    expect(lights(initialState, action)).toMatchObject(wantedState);
+});
