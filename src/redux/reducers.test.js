d6a5cd61b5226ee687fda6a17cc7d4f061ec99f4
Mico Piira <mico.piira@gmail.com>
2017-08-03 21:08:18 +0300

    Fix SETLIGHT_STATE reducer test

--- src/redux/reducers.test.js
+++ src/redux/reducers.test.js
@@ -4,8 +4,8 @@ import { setLightState } from './actions';
 
 
 it('sets light state', () => {
-    const initialState = {1: {state: {a: 'b', c: 'd'}}};
-    const wantedState = {1: {state: {a: 'a', c: 'd'}}};
+    const initialState = {1: {state: {a: 'b', c: 'd'}, e: {f: 'g'}}};
+    const wantedState = {1: {state: {a: 'a', c: 'd'}, e: {f: 'g'}}};
     const action = setLightState(1, {a: 'a'});
     expect(lights(initialState, action)).toMatchObject(wantedState);
 });
