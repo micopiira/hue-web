f649a39e5b958d1c8bfd9997284e17f665c23e67
Mico Piira <mico.piira@gmail.com>
2017-08-09 23:20:16 +0300

    Add react-storybook to develop components in isolation

---
+++ src/stories/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+import ColorPicker from '../components/ColorPicker/ColorPicker';
+import { Button, Welcome } from '@storybook/react/demo';
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('Button', module)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+
+
+storiesOf('Color pickers', module)
+    .add('ColorPicker', () => <ColorPicker onChange={console.log}/>);
