5c2a0203ffa78e13f4cff222005652a25eff98c2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-29 13:46:35 +0000

    Deploy micopiira/hue-web to github.com/micopiira/hue-web.git:gh-pages

---
+++ precache-manifest.8d7f82f24823cf1e635c6ac936cf1160.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f37ba1c9b3c2cc0e20eda128cf0042eb",
+    "url": "/hue-web/index.html"
+  },
+  {
+    "revision": "88c603c089100be3bbd3",
+    "url": "/hue-web/static/css/2.4244d7fd.chunk.css"
+  },
+  {
+    "revision": "88c603c089100be3bbd3",
+    "url": "/hue-web/static/js/2.379fdcf6.chunk.js"
+  },
+  {
+    "revision": "a2282e686194c7fae435",
+    "url": "/hue-web/static/js/main.dcf6f68c.chunk.js"
+  },
+  {
+    "revision": "04302740234d76a0e9ef",
+    "url": "/hue-web/static/js/runtime~main.c707eddb.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/hue-web/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/hue-web/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/hue-web/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/hue-web/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/hue-web/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
